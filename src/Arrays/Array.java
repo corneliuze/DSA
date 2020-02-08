8aa1ace6c4a03f90aa15f3f39043f12eaef2d010
iyanu <falaycornelius@gmail.com>
2020-02-08 21:18:43 +0100

    still on the 2d array

--- src/Arrays/Array.java
+++ src/Arrays/Array.java
@@ -29,22 +29,17 @@ public class Array {
      * 2020/01/18
      * */
 
-    public int[] leftRotation(int[] array){
-        int a = array.length;
-        int d = 3;
-        Arrays.copyOfRange(array, 0, d);
-        int[] tempArray = new int[array.length];
-        int j = 0;
-        for(int i = d; i < array.length; i++ ){
-            tempArray[j] = array[i];
-            j++;
-        }
-        
-
-
-
-
-    }
+//    public int[] leftRotation(int[] array){
+//        int a = array.length;
+//        int d = 3;
+//        Arrays.copyOfRange(array, 0, d);
+//        int[] tempArray = new int[array.length];
+//        int j = 0;
+//        for(int i = d; i < array.length; i++ ){
+//            tempArray[j] = array[i];
+//            j++;
+//        }
+//    }
 
     public void hfhf(String p){
         p.split("[A-Za-z !,?._'@]+");
@@ -58,10 +53,10 @@ public class Array {
      */
 
     public int hourGlassSum(int[][] arr) {
-        int sum = 0;
         int maxSum = 0;
-        for (int i = 0; i < 6 ; i++) {
-            for (int j = 0; j < 6 ; j++) {
+        for (int i = 0; i < arr.length ; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr.length ; j++) {
                 sum += (arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
                 if (sum > maxSum) {
                     maxSum = sum;
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int q = a.hourGlassSum(new int[][]{{-9, -9, -9, 1, 1, 1}, {0, -9, 0, 4, 3, 2},
                 {-9, -9, -9, 1, 2, 3}, {0, 0, 8, 6, 6, 0}, {0, 0, 0, -2, 0, 0}, {0, 0, 1, 2, 4, 0}});
 
-        System.out.println("result is %s, p" + p);
+        System.out.println("result is %s, p" + q);
     }
 
 }
